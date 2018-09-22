b30a8c7e149ccff8959829744f6b948bb86886a8
Matt Helm <mthelm85@gmail.com>
2018-09-22 16:52:22 -0500

    added chartkick chart

--- src/components/Analyzer.vue
+++ src/components/Analyzer.vue
@@ -1,20 +1,44 @@
 <template lang="html">
   <div class="container">
-    <div class="row">
-      <div class="col mt-3">
+    <div class="row mt-3">
+      <div class="col">
+        <span class="lead">First, set your parameters:</span>
+        <b-form class="mt-3" inline @submit.prevent>
+          <label class="mr-2" for="hypothesis-mean">Hypothesis Mean</label>
+          <b-form-input id="hypothesis-mean" type="number" class="mr-2" v-model="hypMean" default="5"></b-form-input>
+          <label class="mr-2" for="viol-rate">Min Viol Rate</label>
+          <b-form-input id="viol-rate" type="number" class="mr-2" v-model="violRateCutoff" default="0.8"></b-form-input>
+          <label class="mr-2" for="proportion-cutoff">Max Proportion</label>
+          <b-form-input id="proportion-cutoff" type="number" class="mr-2" v-model="proportionCutoff" default="0.8"></b-form-input>
+        </b-form>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col lead">
+        Then, upload your report and push the <span class="font-italic">Analyze</span> button:
+      </div>
+    </div>
+    <div class="row mt-3 d-flex justify-content-center">
+      <div class="col-10">
         <b-form-file v-model="file" :state="Boolean(file)" placeholder="Choose a file..."></b-form-file>
-        <button class="btn btn-dark mt-3" @click="analyze">Analyze</button>
-        <ul>
-          <li v-for="item in goodNaics" :key="item.naics">
-            NAICS: {{ item.naics }}
-            <br>
-            P-VALUE: {{ item.pValue }}
-            <br>
-            # CASES: {{ item.numCases }}
-            <br>
-            VIOLS/CASE: {{ item.violsPerCase }}
-          </li>
-        </ul>
+      </div>
+      <div class="col-2">
+        <button class="btn btn-dark" @click="analyze">Analyze</button>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-12">
+        <span class="lead">Finally, view the results:</span>
+        <bar-chart
+          :min="0"
+          class="mt-3"
+          :data="chartData"
+          :download="true"
+          :stacked="true"
+          height="400px"
+          xtitle="Violations/Case"
+          ytitle="NAICS"
+          :legend="false"></bar-chart>
       </div>
     </div>
   </div>
@@ -33,7 +57,11 @@ export default {
       goodNaics: [],
       testArray: [],
       testCode: null,
-      pVal: null
+      pVal: null,
+      hypMean: 5,
+      proportionCutoff: .02,
+      violRateCutoff: 0.8,
+      chartData: []
     }
   },
 
@@ -62,23 +90,36 @@ export default {
           }
         }
 
-        this.pVal = jStat.ttest(10, jStat.mean(this.testArray), jStat.stdev(this.testArray), this.testArray.length, 1)
-
-        if (this.pVal <= .05 && jStat.mean(this.testArray) >= 8) {
+        this.pVal = jStat.ttest(this.hypMean, jStat.mean(this.testArray), jStat.stdev(this.testArray), this.testArray.length, 1)
+        let noViols = this.testArray.filter(num => num === 0)
+        if (this.pVal <= .05 &&
+          jStat.mean(this.testArray) >= this.hypMean &&
+          (this.testArray.length / this.parsedData.length) < this.proportionCutoff &&
+          (1 - (noViols.length / this.testArray.length)) > this.violRateCutoff) {
           this.goodNaics.push({
             naics: this.testCode,
             pValue: this.pVal,
             numCases: this.testArray.length,
-            violsPerCase: jStat.mean(this.testArray)
+            violsPerCase: jStat.mean(this.testArray),
+            violRate: 1 - (noViols.length / this.testArray.length),
+            proportion: this.testArray.length / this.parsedData.length
           })
         }
-
+      }
+      for (let i = 0; i < this.goodNaics.length; i++) {
+        this.chartData.push([
+          this.goodNaics[i].naics,
+          this.goodNaics[i].violsPerCase.toFixed(2),
+          (this.goodNaics[i].violRate * 100).toFixed(2)
+        ])
       }
     }
   },
 
   watch: {
     file () {
+      this.naicsCodes.length = 0
+      this.parsedData.length = 0
       Papa.parse(this.file, {
         complete: (results) => {
           for (let i = 1; i < results.data.length - 1; i++) {
@@ -93,4 +134,5 @@ export default {
 </script>
 
 <style lang="css">
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
+import VueChartkick from 'vue-chartkick'
+import Chart from 'chart.js'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.use(VueChartkick, { adapter: Chart })
 
 Vue.config.productionTip = false
 
