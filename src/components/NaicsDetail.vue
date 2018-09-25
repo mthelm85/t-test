d121cab5d5ba4f1bd973de3234f02296fda44bba
Matt Helm <mthelm85@gmail.com>
2018-09-24 19:25:55 -0500

    added naics descriptions

--- src/components/NaicsDetail.vue
+++ src/components/NaicsDetail.vue
@@ -1,7 +1,13 @@
 <template lang="html">
   <div class="row">
     <div class="col">
-
+      <div class="card mb-3" v-for="item in naicsInfo" :key="item.code">
+        <div class="card-body">
+          <h5 class="card-title">{{ item.code }}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{ item.title }}</h6>
+          <p class="card-text">{{ item.description[0] }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -9,19 +15,31 @@
 <script>
 import axios from 'axios'
 export default {
-
-  async created () {
-    for (let i = 0; i < this.naics.length; i++) {
-      try {
-        let res = await axios.get(`http://naics.codeforamerica.org/v0/q?year=2012&code=${this.naics[i]}`)
-        console.log(res.data.title)
-      } catch (err) {
-        console.log(err)
-      }
+  data () {
+    return {
+      naicsInfo: []
     }
   },
 
-  props: ['naics']
+  props: ['naics'],
+
+  watch: {
+    async naics () {
+      for (let i = 0; i < this.naics.length; i++) {
+        try {
+          let res = await axios.get(`http://naics.codeforamerica.org/v0/q?year=2012&code=${this.naics[i]}`)
+          console.log(res.data)
+          this.naicsInfo.push({
+            code: res.data.code,
+            title: res.data.title,
+            description: res.data.description
+          })
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    }
+  }
 }
 </script>
 
